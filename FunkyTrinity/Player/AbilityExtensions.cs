46ce779ff8a98973f4c865f7c1d5c64bdf2649f3
BUFORD <sheriffbuford99@gmail.com>
2013-09-21 14:21:26 -0400

    class not being used at the moment

---
+++ FunkyTrinity/Player/AbilityExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FunkyTrinity.Ability
+{
+    public static class AbilitiyExtensions
+    {
+        //public static double SecondsFromLastUse(this ability ability)
+        //{
+        //    return DateTime.Now.Subtract(ability.LastUsed).TotalSeconds;
+        //}
+
+        //public static bool HasDelayExpired(this Ability ability)
+        //{
+        //    return ability.SecondsFromLastUse() >= ability.DelayInSeconds;
+        //}
+    }
+}
