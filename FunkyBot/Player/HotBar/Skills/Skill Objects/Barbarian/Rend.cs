fa0ff403dddeac35ad286a7d63981bf6ad6990e4
BUFORD <sheriffbuford99@gmail.com>
2013-12-27 15:24:18 -0500

    fixed rend to attack one unit

--- FunkyBot/Player/HotBar/Skills/Skill Objects/Barbarian/Rend.cs
+++ FunkyBot/Player/HotBar/Skills/Skill Objects/Barbarian/Rend.cs
@@ -3,14 +3,14 @@ using Zeta.Internals.Actors;
 
 namespace FunkyBot.Player.HotBar.Skills.Barb
 {
-	public class Rend : Skill
-	{
-		public override SNOPower Power
-		{
-			get { return SNOPower.Barbarian_Rend; }
-		}
+    public class Rend : Skill
+    {
+        public override SNOPower Power
+        {
+            get { return SNOPower.Barbarian_Rend; }
+        }
 
-		public override int RuneIndex { get { return Bot.Character.Class.HotBar.RuneIndexCache.ContainsKey(Power) ? Bot.Character.Class.HotBar.RuneIndexCache[Power] : -1; } }
+        public override int RuneIndex { get { return Bot.Character.Class.HotBar.RuneIndexCache.ContainsKey(Power) ? Bot.Character.Class.HotBar.RuneIndexCache[Power] : -1; } }
 
         public override void Initialize()
         {
@@ -23,7 +23,7 @@ namespace FunkyBot.Player.HotBar.Skills.Barb
             PreCast = new SkillPreCast((AbilityPreCastFlags.CheckRecastTimer | AbilityPreCastFlags.CheckEnergy |
                                       AbilityPreCastFlags.CheckCanCast | AbilityPreCastFlags.CheckPlayerIncapacitated));
 
-            ClusterConditions = new SkillClusterConditions(5d, 8, 2, true, 0.90d);
+            ClusterConditions = new SkillClusterConditions(5d, 8, 1, true);
 
             SingleUnitCondition = new UnitTargetConditions(TargetProperties.None, 10,
                 falseConditionalFlags: TargetProperties.DOTDPS);
@@ -31,26 +31,26 @@ namespace FunkyBot.Player.HotBar.Skills.Barb
             FcriteriaCombat = () => true;
         }
 
-		#region IAbility
-		public override int GetHashCode()
-		{
-			return (int)Power;
-		}
-		public override bool Equals(object obj)
-		{
-			//Check for null and compare run-time types. 
-			if (obj == null || GetType() != obj.GetType())
-			{
-				return false;
-			}
-			else
-			{
-				Skill p = (Skill)obj;
-				return Power == p.Power;
-			}
-		}
+        #region IAbility
+        public override int GetHashCode()
+        {
+            return (int)Power;
+        }
+        public override bool Equals(object obj)
+        {
+            //Check for null and compare run-time types. 
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                Skill p = (Skill)obj;
+                return Power == p.Power;
+            }
+        }
 
 
-		#endregion
-	}
+        #endregion
+    }
 }
