c2ec5e82697af38d5fc9b7d7aff1b119e03e55d4
BUFORD <sheriffbuford99@gmail.com>
2013-12-30 09:45:03 -0500

    skill useage changes

--- FunkyBot/Player/HotBar/Skills/Skill Objects/Barbarian/Battlerage.cs
+++ FunkyBot/Player/HotBar/Skills/Skill Objects/Barbarian/Battlerage.cs
@@ -3,45 +3,45 @@ using Zeta.Internals.Actors;
 
 namespace FunkyBot.Player.HotBar.Skills.Barb
 {
-	 public class Battlerage : Skill
-	 {
-		 public override SNOPower Power
-		  {
-				get { return SNOPower.Barbarian_BattleRage; }
-		  }
+    public class Battlerage : Skill
+    {
+        public override SNOPower Power
+        {
+            get { return SNOPower.Barbarian_BattleRage; }
+        }
 
-		  public override int RuneIndex { get { return Bot.Character.Class.HotBar.RuneIndexCache.ContainsKey(Power)?Bot.Character.Class.HotBar.RuneIndexCache[Power]:-1; } }
+        public override int RuneIndex { get { return Bot.Character.Class.HotBar.RuneIndexCache.ContainsKey(Power) ? Bot.Character.Class.HotBar.RuneIndexCache[Power] : -1; } }
 
-		  public override void Initialize()
-		  {
-				Cooldown=118000;
-				ExecutionType=AbilityExecuteFlags.Buff;
-				WaitVars=new WaitLoops(1, 1, true);
-				Cost=20;
-				IsBuff=true;
-				UseageType=AbilityUseage.Anywhere;
-                Priority = AbilityPriority.Low;
-                PreCast = new SkillPreCast(AbilityPreCastFlags.CheckExisitingBuff|AbilityPreCastFlags.CheckEnergy | AbilityPreCastFlags.CheckPlayerIncapacitated | AbilityPreCastFlags.CheckCanCast);
-				FcriteriaBuff=() => true;
-                FcriteriaBuff = () => LastUsedMilliseconds > 30000;
-		  }
+        public override void Initialize()
+        {
+            Cooldown = 100000;
+            ExecutionType = AbilityExecuteFlags.Buff;
+            WaitVars = new WaitLoops(1, 1, true);
+            Cost = 20;
+            IsBuff = true;
+            UseageType = AbilityUseage.Anywhere;
+            Priority = AbilityPriority.Low;
+            PreCast = new SkillPreCast(AbilityPreCastFlags.CheckExisitingBuff | AbilityPreCastFlags.CheckEnergy | AbilityPreCastFlags.CheckPlayerIncapacitated | AbilityPreCastFlags.CheckCanCast);
+            FcriteriaBuff = () => true;
+            FcriteriaBuff = () => LastUsedMilliseconds > 30000;
+        }
 
-		  #region IAbility
-		  public override int GetHashCode()
-		  {
-				return (int)Power;
-		  }
-		  public override bool Equals(object obj)
-		  {
-				//Check for null and compare run-time types. 
-				if (obj==null||GetType()!=obj.GetType())
-				{
-					 return false;
-				}
-			  Skill p=(Skill)obj;
-			  return Power==p.Power;
-		  }
+        #region IAbility
+        public override int GetHashCode()
+        {
+            return (int)Power;
+        }
+        public override bool Equals(object obj)
+        {
+            //Check for null and compare run-time types. 
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Skill p = (Skill)obj;
+            return Power == p.Power;
+        }
 
-		  #endregion
-	 }
+        #endregion
+    }
 }
